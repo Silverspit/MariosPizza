b51d8a2e16872b9200fe27b339d8c3bda0336667
AdamHoppe <adamhoppe98@gmail.com>
2025-03-18 15:29:37 +0100

    add pizzaClass

--- module1/src/Order.java
+++ module1/src/Order.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class Order {
+
+    ArrayList<Pizza> pizzaOrders = new ArrayList<>();
+
+
+    public void addPizza(String pizzaName, int pizzaPrice) {
+
+        pizzaOrders.add(new Pizza(pizzaName,pizzaPrice));
+    }
+    /*public void addPizzaWithTopppings(String pizzaName,int pizzaPrice, ArrayList<Toppings> toppingsName, int toppingsPrice, int nrOftoppings){
+            Pizza p1 = new Pizza(pizzaName,pizzaPrice);
+        for (int i = 0; i < nrOftoppings; i++) {
+            p1.setToppings(toppingsName());
+        }
+            pizzaOrders.add(p1);
+
+    }*/
 }
--- module1/src/Toppings.java
+++ module1/src/Toppings.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class Toppings {
+
+
+    private String name;
+    private int price;
+
+    public int getPrice(){
+        return price;
+    }
 }
