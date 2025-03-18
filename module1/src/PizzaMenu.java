0d0f10525e314b69437633dff620391889d764c1
josefinehoerlin <josefinehoerlin@gmail.com>
2025-03-18 15:38:21 +0100

    pizzamenu

--- module1/src/PizzaMenu.java
+++ module1/src/PizzaMenu.java
@@ -1,2 +1,82 @@
+import java.util.ArrayList;
+
 public class PizzaMenu {
+ private ArrayList<String> names = new ArrayList<>();
+ private ArrayList<Integer> prices = new ArrayList<>();
+
+ // en metode, der tilføjer navne til vores navneliste
+    public void addNames (){
+
+        names.add("Vesuvio: tomatsauce, ost, skinke og oregano");
+        names.add("Amerikaner: tomatsauce, ost, oksefars og oregano");
+        names.add("Cacciatore: tomatsauce, ost, pepperoni og oregano");
+        names.add("Carbons: tomatsauce, ost, kødsauce, spaghetti, cocktailpølser og oregano");
+        names.add("Dennis: tomatsauce, ost, skinke, pepperoni, cocktailpølseer og oregano");
+        names.add("Bertil: tomatsauce, ost, bacon og oregano");
+        names.add("Silvia: tomatsauce, ost, pepperoni, rød peber, løg, oliven og oregano");
+        names.add("Victoria: tomatsauce, ost, ananas, champignon, løg og oregano");
+        names.add("Toronfo: tomatsauce, ost, skinke, bacon, kebab, chili og oregano");
+        names.add("Capricciosa: tomatsauce, ost, skinke, champignon og oregano");
+        names.add("Hawai: tomatsauce, ost, skinke, ananas og oregano");
+        names.add("Le Blissola: tomatsauce, ost, skinke, rejer og oregano");
+        names.add("Venezia: tomatsauce, ost, skinke, bacon og oregano");
+        names.add("Mafia: tomatsauce, ost, pepperoni, bacon, løg og oregano");
+        names.add("Kebab: tomatsauce, ost, kebab, salat og dressing");
+        names.add("Tun Pizza: tomatsauce, ost, tun, løg citronsaft og persille");
+        names.add("Italiano: kødsauce og løg");
+        names.add("Sivas: tomatsauce, ost, skinke, paprika og chili");
+        names.add("Sucuk: tomatsauce, ost, hvidløgspølse, æg og paprika");
+        names.add("Spinatpizza: tomatsauce, ost, spinat, æg, løg og hvidløg");
+        names.add("Kylling: tomatsauce, ost, kylling, salat og dressing");
+        names.add("Vegetar: oliven, champignon, løg og paprika");
+        names.add("Abis: tomatsauce, ost, kødstrimler, løg, champignon, paprika, gorgonzola og hvidløg");
+        names.add("Osman Special: tomatsauce, ost, bernaise, kødstrimler, løg, æg og paprika");
+        names.add("Oskar Special: gorgonzola, mozzarella, gedeost og strudseæg");
+        names.add("Stig Special: alle Arlas produkter");
+        names.add("Mario: tomatsauce, ost, skinke, rejer, champignon og artiskok");
+        names.add("Alfonso: tomatsauce, ost, hakket oksekød, feta og chili");
+        names.add("Abdul: tomatsauce, ost, kylling med karry, oliven, jalapeños og pesto");
+        names.add("Fiesta: tomatsauce, ost, oskesharwarma, hvidløg, jalapeños, salat og dressing");
+        names.add("Rucola: tomatsauce, ost, pepperoni, rucola og pesto");
+
+    }
+    // en metode, der tilføjer pris til vores prisliste
+    public void addPrices() {
+        prices.add(57);
+        prices.add(53);
+        prices.add(57);
+        prices.add(63);
+        prices.add(65);
+        prices.add(57);
+        prices.add(61);
+        prices.add(61);
+        prices.add(61);
+        prices.add(61);
+        prices.add(57);
+        prices.add(61);
+        prices.add(61);
+        prices.add(61);
+        prices.add(61);
+        prices.add(55);
+        prices.add(78);
+        prices.add(55);
+        prices.add(61);
+        prices.add(61);
+        prices.add(57);
+        prices.add(61);
+        prices.add(110);
+        prices.add(110);
+        prices.add(110);
+        prices.add(61);
+        prices.add(78);
+        prices.add(67);
+        prices.add(55);
+        prices.add(78);
+        prices.add(90);
+
+    }
+
+
+
+
 }
