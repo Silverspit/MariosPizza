f054b2ec4bb110026c30af371d5855bdf5fcca98
AdamHoppe <adamhoppe98@gmail.com>
2025-03-19 09:15:32 +0100

    fixed some ordersystem

--- module1/src/OrderSystem.java
+++ module1/src/OrderSystem.java
@@ -2,17 +2,49 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class OrderSystem {
-   private ArrayList<Order> orders = new ArrayList<>();
+    private static ArrayList<Order> orders = new ArrayList<>();
 
 
-   // Metode der placerer order i listen
-    public void placeOrder(String name, ArrayList<String> pizzas, ArrayList<Integer> price, Scanner scanner){
+    // Metode der placerer order i listen
+    public static void placeOrder(String name, ArrayList<String> pizzas, ArrayList<Integer> price, Scanner scanner) {
         Order order = new Order(name);
 
 
-       // loop der tilføjer pizza til din ordre
+        // loop der tilføjer pizza til din ordre
+        boolean placingOrder = true;
+        while (placingOrder) {
+            System.out.println("Hvad for en pizza vil du tilføje til din ordre? ");
+            for (int i = 0; i < pizzas.size(); i++) {
+                System.out.println((i + 1) + ": " + pizzas.get(i));
+            }
+            int choice = scanner.nextInt();
 
-       while (true)  {
+            if (choice > 0 && choice < (pizzas.size() + 1)) {
+                order.addPizza(pizzas.get(choice), price.get(choice));
+            } else {
+                System.out.println("not a pizza we make");
+            }
+            boolean anotherPizza = true;
+            while (anotherPizza) {
+                System.out.println("would you like to add another pizza? y/n");
+                switch (scanner.nextLine().trim().toLowerCase()) {
+                    case "y":
+                        break;
+                    case "n": {
+                        anotherPizza = false;
+                        placingOrder = false;
+                        break;
+                    }
+                    default: {
+                        System.out.println("wrong input");
+                    }
+                }
+            }
+
+
+        }
+
+       /*while (true)  {
            System.out.println("Hvad for en pizza vil du tilføje til din ordre? ");
            for (int i = 0; i < pizzas.size() ; i++) {
                System.out.println((i + 1 ) + ": " + pizzas.get(i));
@@ -110,12 +142,12 @@ public class OrderSystem {
                }
            }
            order.addPizza(name, price);
-       }
+       }*/
         orders.add(order);
 
     }
 
-   // Metode Fjern ordre fra listen
+    // Metode Fjern ordre fra listen
 
 
     // metode Afslutte ordre
